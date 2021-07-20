d4fc8fc8f751b26c965eda5a1a61efe64d11a500
nagehancapa <capanagehan@gmail.com>
2021-07-20 20:03:54 +0200

    added custom hook example

--- src/components/AddNoteForm.js
+++ src/components/AddNoteForm.js
@@ -1,10 +1,12 @@
 import React, { useState, useContext } from "react";
 import NotesContext from "../context/notes-context";
+import useMousePosition from "../hooks/useMousePosition";
 
 const AddNoteForm = () => {
   const { notesDispatch } = useContext(NotesContext);
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
+  const position = useMousePosition();
 
   const addNote = (e) => {
     e.preventDefault();
@@ -14,9 +16,10 @@ const AddNoteForm = () => {
   };
 
   return (
-    <div>
-      {" "}
-      <p>Add note</p>
+    <>
+      <p>
+        Add note {position.x} - {position.y}
+      </p>
       <form onSubmit={addNote}>
         <input value={title} onChange={(e) => setTitle(e.target.value)} />
         <textarea
@@ -25,7 +28,7 @@ const AddNoteForm = () => {
         ></textarea>
         <button>add note</button>
       </form>
-    </div>
+    </>
   );
 };
 
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,8 +1,10 @@
 import React, { useContext } from "react";
 import NotesContext from "../context/notes-context";
+import useMousePosition from "../hooks/useMousePosition";
 
 const Note = ({ note }) => {
   const { notesDispatch } = useContext(NotesContext);
+  const position = useMousePosition();
 
   const removeNote = (title) => {
     notesDispatch({ type: "REMOVE_NOTE", title });
@@ -12,6 +14,9 @@ const Note = ({ note }) => {
     <div>
       <h3>{note.title}</h3>
       <p>{note.body}</p>
+      <p>
+        {position.x}, {position.y}
+      </p>
       <button onClick={() => removeNote(note.title)}>x</button>
     </div>
   );
